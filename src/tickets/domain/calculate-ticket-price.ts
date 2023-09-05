68e5eb4ba8f91f36226913452db047d86f231529
kchnr <jean.kirchner@gmail.com>
2023-09-05 16:05:07 -0300

    blah

--- src/tickets/domain/calculate-ticket-price.ts
+++ src/tickets/domain/calculate-ticket-price.ts
@@ -1,18 +1,9 @@
 /*
 - Adult: $ 40.00
-- Kids(0 / 7): $ 15.00
-- Students(Student Card): $ 20.00
-- Senior(high than 65 years old): $ 20.00
 */
 
 enum WEEKDAY {
-  SUNDAY,
   MONDAY,
-  TUESDAY,
-  WEDNESDAY,
-  THURSDAY,
-  FRIDAY,
-  SATURDAY,
 }
 
 const calculateTicketPrices = (numberOfAdults: number, weekday: string) => {
@@ -30,19 +21,7 @@ function calculateTicketForAdults(numberOfAdults: number, weekday: WEEKDAY) {
   const adultPrice = 40.0;
 
   switch (weekday) {
-    case WEEKDAY.SUNDAY:
-      return 0;
     case WEEKDAY.MONDAY:
-      return 0;
-    case WEEKDAY.TUESDAY:
-      return 0;
-    case WEEKDAY.WEDNESDAY:
-      return 0;
-    case WEEKDAY.THURSDAY:
-      return 0;
-    case WEEKDAY.FRIDAY:
-      return 0;
-    case WEEKDAY.SATURDAY:
       return 0;
   }
 }
